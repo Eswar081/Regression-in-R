36c14448da155f7e9ad9bf1fda1cc31c7b2109c4
Eswar081 <54735811+Eswar081@users.noreply.github.com>
2019-09-07 17:21:06 +0530

    Add files via upload

---
+++ PCA.R
@@ -0,0 +1,118 @@
+7 + (3*2)
+8*(5+8+8)
+2^3
+sqrt(9)
+log(9)
+log(10)
+myname <- data
+objects()
+rm()
+rm(list = (ls))
+help.start(data)
+library()
+2-(9*6)/3*6
+sqrt(25)/23*29
+23*29/sqrt(2)
+2*3
+100*12*18*20
+v <- c(1,2,3)
+v
+<-(,2)
+class(v)
+ES <- seq(1, 5, by = 2)
+
+seq(1,10, by=2)
+seq(1, 10, by = 1)
+seq(1, 20, length = 4)
+rep(2,3)
+rep(3,4)
+rep(2,10)
+rep(TRUE,4)
+rep(1:5,6)
+rep(1:3, each = 3)
+v1 <- c(1, 2, 3)
+v2 <- c(5, 3, 4)
+v1 + v2
+v1-v2
+v1+v2
+v1/v2
+Data <- ES
+ES <- c(1,2,3)
+x <- c(1,2,3)
+sum(x)
+prod(x)
+sort(x)
+min(x)
+max(x)
+length(x)
+mean(x)
+median(x)
+mode(x)
+a <- matrix(2 : 4, nrow = 3, ncol = 5)
+class(x)
+a
+typeof(a)
+p <- matrix(10:15, nrow = 4, ncol = 3, byrow = TRUE )
+p
+dim(p)
+e <- 1:3
+p <- 4:6
+MI <- cbind(e,p)
+MI
+V <- 7:10
+U <- 10:13
+M2 <- cbind(V,U)
+M2
+M2 <- rbind(V,U)
+M2
+MI + M2
+A <- array(data = 1:10, dim = 3, 2, 4)
+name <- c("eswar", "Vishi", "pavani")
+age <- c(34, 5, 28)
+dat <- cbind(name, age)
+dat
+dat <-rbind(name, age)
+dat
+x <- c(1,2,3,3)
+y <- c(5,4,6,5)
+data <- data.frame(x, y)
+class(data)
+attributes(data)
+data <- c(wc.at)
+attributes(data)
+data <-(`Cars(1)`)
+data<-`Cars(1)`[,c(1,2,3)]
+pairs(data)
+cor(data)
+plot(data)
+avPlots(model.car)
+ep <- c(22,56,22,11,1,1,2,2,33,3,3)
+fep <- factor(ep)
+fep
+class(ep)
+unclass(ep)
+attr(,"ep")
+mylist <- list(ep, data, MI)
+mylist
+x <- rep(1:3, 5)
+rep
+summary(x)
+class(x)
+xf <- factor(x)
+class(xf)
+summary(xf)
+class(AirPassengers)
+attributes(AirPassengers)
+class(MSFT)
+v <- c(2, 3, 4, 5)
+v[3]
+v <- c(cars)
+v[2]
+v[6]
+v[3]
+v[4]
+v[1]
+v <-c(`Cars(1)`)
+v[1]
+v[4]
+v[c(1, 3)]
